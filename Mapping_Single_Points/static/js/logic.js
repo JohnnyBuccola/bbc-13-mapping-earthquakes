ab07d9e7853e494749de28ba20fd9c175ec6b834
Johnny Buccola <jbuccola@projectfrog.com>
2021-09-03 15:39:13 -0700

    fixed single_points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// //  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -15,8 +15,8 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map);
 
 // Add a circle to a map
-// L.circleMarker([34.0522, -118.2437], {
-//     radius: 100,
-//     color: "black",
-//     fillColor: "#ffffa1"
-//  }).addTo(map);
+L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    color: "black",
+    fillColor: "#ffffa1"
+ }).addTo(map);
